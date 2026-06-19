476cd239c7585f4c25f48d09d80b608819a87ada
Administrator <Administrator@WIN-PPG2V3623SE>
2026-06-19 18:08:21 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Connector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace ConsoleApp12
+{
+    class Connector
+    {
+        string connection_string;
+        SqlConnection connection;
+        public Connector(string connection_string)
+        {
+            this.connection_string = connection_string;
+            connection = new SqlConnection(connection_string);
+        }
+        public void Select(string cmd)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand(cmd, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            for (int i = 0; i < reader.FieldCount; i++)
+                Console.Write(reader.GetName(i)+"\t");
+            Console.WriteLine();
+            while(reader.Read())
+            {
+                for (int i = 0;i < reader.FieldCount;i++)
+                    Console.Write($"{reader[i]}\t\t");
+                Console.WriteLine();
+            }
+            reader.Close();
+            connection.Close();
+        }
+       public void Select(string fields,string tables,string condition=" ")
+        {
+                string cmd = $"SELECT {fields} FROM {tables}";
+                if (condition != " ") cmd += $"WHERE {condition}";
+                cmd += ";";
+        }
+       public object Scalar(string cmd)
+        {
+            object result = null;
+            connection.Open();
+            SqlCommand command = new SqlCommand(cmd, connection);
+            result = command.ExecuteNonQuery();
+            connection.Close();
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            Connector connector = new Connector(connection_string);
+
+            string cmd = "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
+            connector.Select(cmd);
+            Console.WriteLine($"Kolichestvo zapisei:{connector.Scalar("SELECT COUNT (*) FROM Movies")}");
+            connector.Select("SELECT * FROM Directors");
+            Console.WriteLine($"Kolichestvo zapisei:{connector.Scalar("SELECT COUNT (*) FROM Directors")}");
+            //command.CommandText = "SELECT COUNT(*) FROM Movies";
+            //Console.WriteLine($"Kolichestvo zapisei:\t{command.ExecuteScalar()}");
+            //connection.Close();
+        }
+       
+    }
+}
