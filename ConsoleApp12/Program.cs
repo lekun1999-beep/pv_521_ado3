118e82930018be561bd4eb4c6b9c96085b02934c
Administrator <Administrator@WIN-PPG2V3623SE>
2026-06-21 00:23:27 +0700

    GetPrimaryKeyColumnName

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -12,12 +12,19 @@ namespace ConsoleApp12
         {
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             Connector connector = new Connector(connection_string);
+            Console.WriteLine(connector.GetPrimaryKeyColumnName("Directors"));
+            Console.WriteLine(connector.GetPrimaryKeyColumnName("Movies"));
+            //connector.Insert($@"INSERT Directors (director_id, first_name, last_name) 
+            //VALUES ({connector.GetNextPrimaryKey("Directors")}, N'Guy', N'Richie');");
+            connector.Insert("Directors", "director_id, first_name, last_name", 
+                $"{connector.GetNextPrimaryKey("Directors")},John,Singleton");
+            Console.WriteLine($"PK Max:\t{connector.GetMaxPrimaryKey("Directors")}");
+            //string cmd = "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
+            connector.Select("*", "Directors");            
+            Console.WriteLine($"Kolichestvo zapisei: {connector.Scalar("SELECT COUNT (*) FROM Directors")}");
+            connector.Select("title, release_date, first_name, last_name", "Movies, Directors", "director = director_id");
+            Console.WriteLine($"Kolichestvo zapisei: {connector.Scalar("SELECT COUNT (*) FROM Movies")}");
 
-            string cmd = "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
-            connector.Select(cmd);
-            Console.WriteLine($"Kolichestvo zapisei:{connector.Scalar("SELECT COUNT (*) FROM Movies")}");
-            connector.Select("SELECT * FROM Directors");
-            Console.WriteLine($"Kolichestvo zapisei:{connector.Scalar("SELECT COUNT (*) FROM Directors")}");
             //command.CommandText = "SELECT COUNT(*) FROM Movies";
             //Console.WriteLine($"Kolichestvo zapisei:\t{command.ExecuteScalar()}");
             //connection.Close();
