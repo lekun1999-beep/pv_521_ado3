25f04871432c09f52b54013ab475dca4941c486e
Administrator <Administrator@WIN-PPG2V3623SE>
2026-06-22 10:10:54 +0700

    Students Empty fields DONE

--- Academy/MainForm.cs
+++ Academy/MainForm.cs
@@ -82,7 +82,8 @@ namespace Academy
         private void buttonAddStudent_Click(object sender, EventArgs e)
         {
             studentForm = new StudentForm();
-            studentForm.ShowDialog();
+            if (studentForm.ShowDialog() == DialogResult.OK)
+                tabControl_SelectedIndexChanged(tabControl, null);
         }
     }
 }
